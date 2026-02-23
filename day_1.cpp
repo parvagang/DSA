baac44e48306f95c70d02b0e4f2dc0cad2a6d59d
parvagang <parvagang9@gmail.com>
2026-02-23 18:37:12 +0530

    Refactor prime number check logic to use boolean flag instead of count

--- day_1.cpp
+++ day_1.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main() {
     int n,i,count=0;
+    char isPrime=true;
     cout<<"enter a number: ";
     cin>>n;
     for (i=2;i<n;i++){
         if(n%i==0){
-            count+=1;
+            isPrime=false;
+            break;
         }
         }  
-    if(count==0){
+    if(isPrime==true){
         cout<<n<<" is a prime number";
     }
     else{
