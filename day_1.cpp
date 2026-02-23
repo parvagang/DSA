88ee433326bc1a05f35f38e8c671c9561d83c1dc
parvagang <parvagang9@gmail.com>
2026-02-23 18:44:39 +0530

    Add factorial calculation functionality to day_1.cpp

--- day_1.cpp
+++ day_1.cpp
@@ -32,5 +32,17 @@ int main() {
 
     }
     cout<<"sum of numbers which are divisible by 3 from 1 to "<<n<<" is: "<<sum;
+
+
+
+
+    int n,i=1,factorial=1;
+    cout<<"enter a number: ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        factorial=factorial*i;
+    }
+    cout<<"factorial of "<<n<<" is: "<<factorial;
+
     return 0;
 }
