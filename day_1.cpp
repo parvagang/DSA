b7ebf29afebdb5fc801f51e474b9bac49031d25d
parvagang <parvagang9@gmail.com>
2026-02-25 17:34:16 +0530

    Refactor day_1.cpp to implement nCr calculation and remove prime number check functionality

--- day_1.cpp
+++ day_1.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
 using namespace std;
+
+int factorial(int n){
+    int fact = 1;;
+    for(int i = 1; i <= n ; i++){
+        fact *= i;
+    }
+    return fact;
+}
+int nCr(int n, int r){
+    int a = factorial(n);
+    int b = factorial(r);
+    int c = factorial(n-r);
+    int d = a / (b * c);
+    return d;
+
+}
+
+
+
 int main() {
-    int n,i,count=0;
-    char isPrime=true;
-    cout<<"enter a number: ";
-    cin>>n;
-    for (i=2;i<n;i++){
-        if(n%i==0){
-            isPrime=false;
-            break;
-        }
-        }  
-    if(isPrime==true){
-        cout<<n<<" is a prime number";
-    }
-    else{
-        cout<<n<<" is not a prime number";
-    }
-
-
-
-
-
-    int n,i=1,sum=0;
-    cout<<"enter a number: ";
-    cin>>n;
-    for(i=1;i<=n;i++){
-        if(i%3==0){
-            sum=sum+i;
-        }
-
-    }
-    cout<<"sum of numbers which are divisible by 3 from 1 to "<<n<<" is: "<<sum;
-
-
-
-
-    int n,i=1,factorial=1;
-    cout<<"enter a number: ";
-    cin>>n;
-    for(i=1;i<=n;i++){
-        factorial=factorial*i;
-    }
-    cout<<"factorial of "<<n<<" is: "<<factorial;
+    
+    
 
     return 0;
 }
